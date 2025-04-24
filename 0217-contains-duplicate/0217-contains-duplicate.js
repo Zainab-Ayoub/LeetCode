2ac3b7d3b15efb06f2d23cb45c080d7bb1721769
Zainab <151748568+Zainab-Ayoub@users.noreply.github.com>
2025-04-24 22:19:56 +0500

    Time: 67 ms (12.44%), Space: 78.5 MB (5.05%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = {};
+    for(let i=0; i<nums.length; i++){
+        map[nums[i]] = (map[nums[i]] || 0) + 1;
+    }
+    for(let i=0; i<nums.length; i++){
+        if(map[nums[i]] >= 2){
+            return true;
+        }
+    }
+    return false;
+};
