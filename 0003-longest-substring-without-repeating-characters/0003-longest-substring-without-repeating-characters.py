18155a841e9fa49324cec9215dfa2df8a1f1f2d8
Zainab <151748568+Zainab-Ayoub@users.noreply.github.com>
2025-10-01 15:21:30 +0500

    Time: 8 ms (97.59%), Space: 17.8 MB (52.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,9 +3,10 @@ class Solution:
         map = {}
         left = 0
         maxlength = 0
+
         for right, char in enumerate(s):
             if char in map and map[char] >= left:
                 left = map[char] + 1
             map[char] = right
             maxlength = max(maxlength, right-left + 1)
-        return maxlength        
+        return maxlength      
