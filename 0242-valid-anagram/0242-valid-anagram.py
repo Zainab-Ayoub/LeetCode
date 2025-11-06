0d9289056bfdbd3081a1e0c2cf889b53996d1d0b
Zainab <151748568+Zainab-Ayoub@users.noreply.github.com>
2025-11-06 23:48:30 +0500

    Time: 15 ms (41.03%), Space: 18.7 MB (11.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,6 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        # return Counter(s) == Counter(t) it's a hashmap
-
+    def isAnagram(self, s: str, t: str) -> bool:    
         if len(s) != len(t):
             return False
-       
-        freq = {}
-        for char in s:
-            freq[char] = freq.get(char, 0) + 1
 
-        for char in t:
-            if char not in freq:
-                return False
-
-            freq[char] -= 1
-            if freq[char] < 0:
-                return False
-        return True            
-        
+        return sorted(s) == sorted(t)
